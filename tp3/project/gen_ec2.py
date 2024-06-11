c6a66ea452cae6a6a2676d2f38b6146456294292
Tom <tomfcz.development@gmail.com>
2024-06-11 13:00:12 +0200

    add creation of traffic mirroring

--- tp3/project/gen_ec2.py
+++ tp3/project/gen_ec2.py
@@ -56,7 +56,7 @@ def wait_for_instance_state(ec2, instance_id, desired_state='running', interval=
 ##########
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
- 
+
 serveurweb_instance = create_ec2_instance(ec2, 'ami-00beae93a2d981137', 't3.medium', SECURITY_PUB_ID, SUBNET_PUB_ID, True)
 serveurweb_instance_id = serveurweb_instance['InstanceId']
 
@@ -72,17 +72,61 @@ wait_for_instance_state(ec2, serveurmir_instance_id)
 
 response = ec2.describe_instances(InstanceIds=[serveurweb_instance_id])
 hostname_webserver = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+interface_webserver = response['Reservations'][0]['Instances'][0]['NetworkInterfaces'][0]['NetworkInterfaceId']
 
 response = ec2.describe_instances(InstanceIds=[serveurbdd_instance_id])
 hostname_dbserver = response['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+interface_dbserver = response['Reservations'][0]['Instances'][0]['NetworkInterfaces'][0]['NetworkInterfaceId']
 
 response = ec2.describe_instances(InstanceIds=[serveurmir_instance_id])
 hostname_mirserver = response['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+interface_mirserver = response['Reservations'][0]['Instances'][0]['NetworkInterfaces'][0]['NetworkInterfaceId']
 
 info = {
     'hostname_webserver': hostname_webserver,
+    'interface_webserver': interface_webserver,
+
     'hostname_dbserver': hostname_dbserver,
-    'hostname_mirserver': hostname_mirserver
+    'interface_dbserver': interface_dbserver,
+
+    'hostname_mirserver': hostname_mirserver,
+    'interface_mirserver': interface_mirserver
 }
 with open('instance_infos.json', 'w', encoding='utf-8') as file: file.write(json.dumps(info))
 print(info)
+
+traffic_mirror_target = ec2.create_traffic_mirror_target(
+    NetworkInterfaceId=interface_mirserver
+)
+traffic_mirror_target_id = traffic_mirror_target['TrafficMirrorTarget']['TrafficMirrorTargetId']
+print(f'Create Traffic Mirror Target with ID {traffic_mirror_target_id}')
+
+
+traffic_mirror_filter = ec2.create_traffic_mirror_filter()
+traffic_mirror_filter_id = traffic_mirror_filter['TrafficMirrorFilter']['TrafficMirrorFilterId']
+print(f'Create Traffic Mirror Filter with ID {traffic_mirror_filter_id}')
+
+traffic_mirror_filter_rule = ec2.create_traffic_mirror_filter_rule(
+    TrafficMirrorFilterId=traffic_mirror_filter_id,
+    TrafficDirection='INGRESS',
+    RuleNumber=1,
+    RuleAction='accept',
+    Protocol=6,
+    DestinationPortRange={
+        'FromPort': 80,
+        'ToPort': 80
+    },
+    DestinationCidrBlock='0.0.0.0/0',
+    SourceCidrBlock='0.0.0.0/0'
+)
+traffic_mirror_filter_rule_id = traffic_mirror_filter_rule['TrafficMirrorFilterRule']['TrafficMirrorFilterRuleId']
+print(f'Create Traffic Mirror Filter Rule with ID {traffic_mirror_filter_rule_id}')
+
+traffic_mirror_session = ec2.create_traffic_mirror_session(
+    NetworkInterfaceId=interface_webserver,
+    TrafficMirrorTargetId=traffic_mirror_target_id,
+    TrafficMirrorFilterId=traffic_mirror_filter_id,
+    SessionNumber=1
+)
+traffic_mirror_session_id = traffic_mirror_session['TrafficMirrorSession']['TrafficMirrorSessionId']
+print(f'Create Traffic Mirror Session with ID {traffic_mirror_session_id}')
